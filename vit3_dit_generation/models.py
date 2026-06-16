95077711c5f291e88d7f6340bb4ba9c30828cfd5
Liyining-ai <161810120+Liyining-ai@users.noreply.github.com>
2026-06-16 14:58:43 +0800

    Update models.py

--- vit3_dit_generation/models.py
+++ vit3_dit_generation/models.py
@@ -334,46 +334,46 @@ def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
 #                                   DiT Configs                                  #
 #################################################################################
 
-def DiT_XL_2(**kwargs):
+def DiT3_XL_2(**kwargs):
     return DiT(depth=28, hidden_size=1152, patch_size=2, num_heads=16, **kwargs)
 
-def DiT_XL_4(**kwargs):
+def DiT3_XL_4(**kwargs):
     return DiT(depth=28, hidden_size=1152, patch_size=4, num_heads=16, **kwargs)
 
-def DiT_XL_8(**kwargs):
+def DiT3_XL_8(**kwargs):
     return DiT(depth=28, hidden_size=1152, patch_size=8, num_heads=16, **kwargs)
 
-def DiT_L_2(**kwargs):
+def DiT3_L_2(**kwargs):
     return DiT(depth=24, hidden_size=1024, patch_size=2, num_heads=16, **kwargs)
 
-def DiT_L_4(**kwargs):
+def DiT3_L_4(**kwargs):
     return DiT(depth=24, hidden_size=1024, patch_size=4, num_heads=16, **kwargs)
 
-def DiT_L_8(**kwargs):
+def DiT3_L_8(**kwargs):
     return DiT(depth=24, hidden_size=1024, patch_size=8, num_heads=16, **kwargs)
 
-def DiT_B_2(**kwargs):
+def DiT3_B_2(**kwargs):
     return DiT(depth=12, hidden_size=768, patch_size=2, num_heads=12, **kwargs)
 
-def DiT_B_4(**kwargs):
+def DiT3_B_4(**kwargs):
     return DiT(depth=12, hidden_size=768, patch_size=4, num_heads=12, **kwargs)
 
-def DiT_B_8(**kwargs):
+def DiT3_B_8(**kwargs):
     return DiT(depth=12, hidden_size=768, patch_size=8, num_heads=12, **kwargs)
 
-def DiT_S_2(**kwargs):
+def DiT3_S_2(**kwargs):
     return DiT(depth=12, hidden_size=384, patch_size=2, num_heads=6, **kwargs)
 
-def DiT_S_4(**kwargs):
+def DiT3_S_4(**kwargs):
     return DiT(depth=12, hidden_size=384, patch_size=4, num_heads=6, **kwargs)
 
-def DiT_S_8(**kwargs):
+def DiT3_S_8(**kwargs):
     return DiT(depth=12, hidden_size=384, patch_size=8, num_heads=6, **kwargs)
 
 
-DiT_models = {
-    'DiT-XL/2': DiT_XL_2,  'DiT-XL/4': DiT_XL_4,  'DiT-XL/8': DiT_XL_8,
-    'DiT-L/2':  DiT_L_2,   'DiT-L/4':  DiT_L_4,   'DiT-L/8':  DiT_L_8,
-    'DiT-B/2':  DiT_B_2,   'DiT-B/4':  DiT_B_4,   'DiT-B/8':  DiT_B_8,
-    'DiT-S/2':  DiT_S_2,   'DiT-S/4':  DiT_S_4,   'DiT-S/8':  DiT_S_8,
+DiT3_models = {
+    'DiT3-XL/2': DiT3_XL_2,  'DiT3-XL/4': DiT3_XL_4,  'DiT3-XL/8': DiT3_XL_8,
+    'DiT3-L/2':  DiT3_L_2,   'DiT3-L/4':  DiT3_L_4,   'DiT3-L/8':  DiT3_L_8,
+    'DiT3-B/2':  DiT3_B_2,   'DiT3-B/4':  DiT3_B_4,   'DiT3-B/8':  DiT3_B_8,
+    'DiT3-S/2':  DiT3_S_2,   'DiT3-S/4':  DiT3_S_4,   'DiT3-S/8':  DiT3_S_8,
 }
